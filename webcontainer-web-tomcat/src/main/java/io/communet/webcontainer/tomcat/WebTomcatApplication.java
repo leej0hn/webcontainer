309a6f92a8670f93caf8698539d64f339ebf471b
李东晓 <lidx@communet.io>
2017-06-21 12:02:03 +0800

    项目初始化

---
+++ webcontainer-web-tomcat/src/main/java/io/communet/webcontainer/tomcat/WebTomcatApplication.java
@@ -0,0 +1,29 @@
+package io.communet.webcontainer.tomcat;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * <p>function:
+ * <p>User: leejohn
+ * <p>Date: 16/7/8
+ * <p>Version: 1.0
+ */
+@SpringBootApplication
+public class WebTomcatApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(WebTomcatApplication.class);
+        application.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        System.out.println("boot successfully");
+        countDownLatch.await();
+    }
+}
---
+++ webcontainer-web-undertow/src/main/java/io/communet/webcontainer/undertow/controller/TestController.java
@@ -0,0 +1,23 @@
+package io.communet.webcontainer.undertow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * <p>function:
+ * <p>User: LeeJohn
+ * <p>Date: 2017/01/11
+ * <p>Version: 1.0
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/page/test")
+    public ModelAndView testPage() {
+        String test = "Test Page , hello world ";
+        System.out.println(test);
+        return new ModelAndView("testPage","test",test);
+    }
+
+}
